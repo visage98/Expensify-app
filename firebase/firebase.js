cde9a4ed56c0c6bca88b6c82d3d7b418863756bd
Deepanshu Bagri <deepanshu.bagri98@gmail.com>
2019-09-02 18:03:38 +0530

    155 Seperated Databases

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,15 +1,15 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCIlmnVKys_JgQt_JHosAnsuhTAqaODnOQ",
-    authDomain: "expensify-9e147.firebaseapp.com",
-    databaseURL: "https://expensify-9e147.firebaseio.com",
-    projectId: "expensify-9e147",
-    storageBucket: "expensify-9e147.appspot.com",
-    messagingSenderId: "923602049577",
-    appId: "1:923602049577:web:8400ee8ed526b34f"
-  };
-
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+  };  
+  
 firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
