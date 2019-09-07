9d8debf285c0ea16df7d89ee401566274b5872fa
Deepanshu Bagri <deepanshu.bagri98@gmail.com>
2019-09-07 23:18:58 +0530

    158 fetch database

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
+import {startSetExpenses} from './actions/expenses'
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 import 'react-dates/initialize';
@@ -18,4 +19,8 @@ const jsx = (
     </Provider>
 );
 
-ReactDOM.render(jsx , document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p> , document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(()=>{
+    ReactDOM.render(jsx , document.getElementById('app'));
+});
--- src/reducers/expenses.js
+++ src/reducers/expenses.js
@@ -18,7 +18,9 @@ export default (state = expenseReducerDefaultState, action) => {
                 } else {
                     return expense;
                 }
-            })
+            });
+        case 'SET_EXPENSES' : 
+            return action.expenses;
         default:
             return state;
     }
